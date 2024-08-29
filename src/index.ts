c8911d93ccd1704b0b9f2995d4c68a2ba09fcbb3
Aiden Cullo <aidencullo@gmail.com>
2024-08-29 10:39:30 -0400

    create a tuple

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 // @ts-check
 
-function average(arr: number[]) {
-  return arr.reduce((a, b) => a + b) / arr.length;
+function average(arr: [string, string, number]) {
+    return arr;
 }
 
-console.log(average([42, 100, 50, 6, 90]));
-console.log(average(6));
 console.log(average(["ten", "6", 10]));
